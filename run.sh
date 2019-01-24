c262a0d18f67be091183066350b993cf644695d2
Hiroaki Mizuguchi <mhiroaki@gmail.com>
2019-01-24 20:53:28 +0900

    change program name

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ BASE="$(readlink -m $(dirname "$0"))"
 if [[ $PATH =~ :(/mnt/[a-z].+)$ ]]; then
   echo "WINPATH=\"${BASH_REMATCH[1]}\"" >"$BASE/env"
 fi
-exec "$BASE/src/main" -d 127.0.0.1 10023 "$BASE/telnetd" -L "$BASE/login"
+exec "$BASE/src/wsltelnetd" -d 127.0.0.1 10023 "$BASE/telnetd" -L "$BASE/login"
